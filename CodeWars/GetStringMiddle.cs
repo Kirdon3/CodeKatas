31a9f39870409d895591e8bf472599d1eff33534
Paweł Mrówczyński <pawel.mrowczynski@outlook.com>
2016-11-09 22:17:41 +0100

    Getting middle of the string Kata completed.

---
+++ CodeWars/GetStringMiddle.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace CodeWars
+{
+    class GetStringMiddle
+    {
+        public static string GetMiddle(string s)
+        {
+            return s.Length%2 == 0 ? s[s.Length/2 -1].ToString() + s[s.Length / 2] : s[s.Length/2].ToString();
+
+            //Probably better:   return s.Substring(s.Length / 2 - (1 - s.Length % 2), 2 - s.Length % 2);
+        }
+
+        [TestFixture]
+        public class GetMiddleTest
+        {
+            [Test]
+            public void GenericTests()
+            {
+                Assert.AreEqual("es", GetStringMiddle.GetMiddle("test"));
+                Assert.AreEqual("t", GetStringMiddle.GetMiddle("testing"));
+                Assert.AreEqual("dd", GetStringMiddle.GetMiddle("middle"));
+                Assert.AreEqual("A", GetStringMiddle.GetMiddle("A"));
+            }
+        }
+    }
+}
