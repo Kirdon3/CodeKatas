b47b3ecf5b56618b078e9d77fa0e13778c48c82f
Paweł Mrówczyński <pawel.mrowczynski@outlook.com>
2016-11-18 00:04:51 +0100

    Initial draft of Tennis Match.

---
+++ TennisKata/Player.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TennisKata
+{
+    class Player
+    {
+        public string playerName { get; set; }
+        public Player(string playerName)
+        {
+            this.playerName = playerName;
+        }
+    }
+}
---
+++ TennisKata/TennisMatch.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TennisKata
+{
+    public class TennisMatch
+    {
+        public TennisMatch(string playerOne, string playerTwo)
+        {
+            this.playerOne = new Player(playerOne);
+            this.playerTwo = new Player(playerTwo); ;
+        }
+
+        private Player playerOne { get; set; }
+        private Player playerTwo { get; set; }
+
+        public bool PlayerOneScored()
+        {
+            playerOne.AddPoint(p2.)
+            return false;
+        }
+
+        public bool PlayerTwoScored()
+        {
+            return false;            
+        }
+    }
+
+    public class TennisGameTests
+    {
+        public void TennisGame_WhenStartingAGame_AGameIsCreated()
+        {
+            
+        }
+    }
+}
