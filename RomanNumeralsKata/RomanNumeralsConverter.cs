b8b6ab59503a0dda3eefebc1c152fe04c8872bfa
Paweł Mrówczyński <pawel.mrowczynski@outlook.com>
2016-12-08 23:54:21 +0100

    Roman numerals converter first test

---
+++ RomanNumeralsKata/RomanNumeralsConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace RomanNumeralsKata
+{
+    public class RomanNumeralsConverter
+    {
+        public double ConvertToArabic(string romanNumber)
+        {
+            return 1;
+        }
+    }
+
+    [TestFixture]
+    public class RomanNumeralsConverterTests
+    {
+        [Test]
+        public void Converting_Number_I_Returns_one()
+        {
+            var numeralsConverter = new RomanNumeralsConverter();
+
+            var result = numeralsConverter.ConvertToArabic("I");
+
+            Assert.AreEqual(1, result);
+        }
+    }
+}
